8a8a4cf11d2291f8c95468d365f54b99929c3820
maks667 <dgorpinuk50@gmail.com>
2023-09-25 23:15:59 +0300

    changed wxTextCtrl to wxStyledTextCtrl, fixed image displacement and size fitting

--- src/Window/Window.cpp
+++ src/Window/Window.cpp
@@ -11,7 +11,7 @@ Window::Window(const wxString& title,const wxPoint& position,const wxSize& win_s
     
     
     wxBoxSizer * main_sizer = new wxBoxSizer(wxHORIZONTAL);
-    wxBoxSizer * left = new wxBoxSizer(wxVERTICAL);
+    left = new wxBoxSizer(wxVERTICAL);
     wxBoxSizer * centre = new wxBoxSizer(wxVERTICAL);
     wxBoxSizer * right = new wxBoxSizer(wxVERTICAL);
 
@@ -19,14 +19,15 @@ Window::Window(const wxString& title,const wxPoint& position,const wxSize& win_s
     wxButton * open = new wxButton(this,wxID_ANY,wxString("Open"),wxPoint((this->win_size.GetWidth()/2)-50,(this->win_size.GetHeight()/2)-25),wxSize(100,50));
     wxButton * convert = new wxButton(this,wxID_ANY,wxString("Convert"),wxPoint(open->GetPosition().x,open->GetPosition().y+50),wxSize(100,50));
 
-    result = new wxTextCtrl(this,wxID_ANY,wxString("Nothing added yet.."),wxPoint(this->win_size.GetWidth()/2+100,50),wxSize(400,600),  wxTE_MULTILINE |wxTE_READONLY | wxHSCROLL);
+    result = new wxStyledTextCtrl(this,wxID_ANY,wxPoint(this->win_size.GetWidth()/2+100,50),wxSize(400,600),  wxTE_MULTILINE  ,wxString("Nothing added yet.."));
+    result->SetEditable(wxTE_READONLY);
     right->Add(result,0,wxEXPAND | wxTOP,20);
-    
-    font = new wxFont(wxSize(16, 16), wxFontFamily::wxFONTFAMILY_TELETYPE, wxFontStyle::wxFONTSTYLE_NORMAL, wxFontWeight::wxFONTWEIGHT_NORMAL);//Monospace font
-    result->SetFont(*font);
     wxInitAllImageHandlers();
+    font = new wxFont(wxSize(16, 16), wxFontFamily::wxFONTFAMILY_TELETYPE, wxFontStyle::wxFONTSTYLE_NORMAL, wxFontWeight::wxFONTWEIGHT_NORMAL);//Monospace font
+    result->StyleSetFont(wxSTC_STYLE_DEFAULT,*font);
+    result->Center();
     image = new wxStaticBitmap(this,wxID_ANY,wxBitmap(REFS "no.png",wxBITMAP_TYPE_PNG),wxPoint(this->win_size.GetWidth()/4-200,this->win_size.GetHeight()/4),wxSize(200, 500));
-    
+    //image = new ScrolledImage(wxString(REFS "no.png"),this,wxID_ANY);
     
     open->Bind(wxEVT_BUTTON,&Window::onButtonOpenClick,this);
     convert->Bind(wxEVT_BUTTON,&Window::onButtonConvertClick,this);
@@ -34,12 +35,13 @@ Window::Window(const wxString& title,const wxPoint& position,const wxSize& win_s
 
     centre->Add(open,1,wxEXPAND | wxTOP,300);
     centre->Add(convert,1,wxEXPAND | wxBOTTOM,300);
+    this->SetAutoLayout(true);
     left->Add(image,1,wxEXPAND | wxALL,5);
+
     main_sizer->Add(left,1,wxEXPAND | wxALL,5);
     main_sizer->Add(centre,1,wxEXPAND | wxALL,5);
     main_sizer->Add(right,0,wxEXPAND | wxALL,5);
     this->SetSizer(main_sizer);
-    //wxLog::SetActiveTarget(new wxLogTextCtrl(result));
     CreateStatusBar();
 }
 
@@ -60,13 +62,15 @@ void Window::onButtonOpenClick(wxCommandEvent& ev){
     }
     wxBitmap tmp;
     tmp.LoadFile(openFileDialog->GetPath(),wxBITMAP_TYPE_PNG);
+    wxSize sizer_size = left->GetSize();
     image->SetBitmap(tmp);
+    image->SetSize(sizer_size);
     wxLogStatus(openFileDialog->GetPath());
     
 }
 
 void Window::onButtonConvertClick(wxCommandEvent& ev){
-    result->Clear();
+    result->ClearAll();
     std::ifstream myfile;
     myfile.open(file_T"/index.txt");
     if(myfile.is_open()){
@@ -82,21 +86,18 @@ void Window::onButtonConvertClick(wxCommandEvent& ev){
     {
         
         
-        *result<<row<<'\n';
+        result->AddText(row+"\n");
     }
     myfile.close();
     delete askii;
 }
 void Window::OnKeyDown(wxMouseEvent &event){
     if(event.GetWheelRotation() >0){
-        font->Scale(1.5);
-        wxLogStatus("+");
+        result->ZoomIn();
     }else if(event.GetWheelRotation() <0){
-        font->Scale(0.5);
-        wxLogStatus("-");
+        result->ZoomOut();
     }
     
-    result->SetFont(*font);
 }
 
 Window::~Window(){}
--- src/Window/Window.hpp
+++ src/Window/Window.hpp
@@ -4,18 +4,20 @@
 #include <iostream>
 #include <fstream>
 #include <wx/wx.h>
-#include <wx/grid.h>
 #include <wx/filedlg.h>
 #include <wx/wfstream.h>
+#include <wx/stc/stc.h>
 
 #include "SFML/ASKII.hpp"
+//#include "Components/ScrolledImage.hpp"
 class Window : public wxFrame
 {
     wxPanel * panel;
     wxSize win_size;
     wxPoint position;
     wxStaticBitmap * image;
-    wxTextCtrl * result;
+    wxStyledTextCtrl * result;
+    wxBoxSizer * left;
     wxFont *font;
     wxFileDialog *openFileDialog;
     askii::ASKII_ALGORITHM * askii;
